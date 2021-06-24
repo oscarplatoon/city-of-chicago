3bc6c53c6cc06f757e50040b5b71c2e2be937534
Marc Stanley <marcson909@gmail.com>
2021-06-24 13:51:43 -0500

    functional database reading csv

--- chicago.py
+++ chicago.py
@@ -3,22 +3,23 @@
 
 import csv
 import psycopg2
-import os
+import getpass
 
 def drop_properties_table():
     return ("DROP TABLE IF EXISTS employees CASCADE;")
     
 # Table creation, revisit if varchar fields are broken.
 def table_creation_query():
-    return ("CREATE TABLE employees (id serial PRIMARY KEY, first_name varchar(255), last_name varchar(255), job_title varchar(60), full_or_part_time varchar, department varchar(255), annual_salary int);")
+    return ("CREATE TABLE employees (id serial PRIMARY KEY, first_name varchar(255), last_name varchar(255), job_title varchar(60), full_or_part_time varchar, department varchar(255), annual_salary integer);")
 
 def clean_data(csv_row):
     cleaned = {}
     # first_name
     # last_name
     ### data comes in as name row['name'] = "Last,  First MI"
-    last_first = csv_row['name'].split(",").trim()
+    last_first = csv_row['Name'].split(",")
     first = last_first[1].split(' ')[0] #this discards Middle Initial
+    first.strip() # in case of any leading whitespace from the last_first split
     cleaned['last_name'] = last_first[0]
     cleaned['first_name'] = first
     # job_title
@@ -36,7 +37,7 @@ def clean_data(csv_row):
         hourly_annual = int(float(csv_row['Typical Hours']) * float(csv_row['Hourly Rate'])* float(50))
         cleaned['annual_salary'] = hourly_annual
     else:
-        cleaned['annual_salary'] = csv_row['Annual Salary']
+        cleaned['annual_salary'] = int(float(csv_row['Annual Salary']))
 
     ## From sac real estate exercise.
     # cleaned['street_address'] = csv_row['street']
@@ -55,10 +56,43 @@ def clean_data(csv_row):
 
 
 connection = psycopg2.connect(
-    f"dbname=chicago_salaries user={os.getlogin()}")
+    f"dbname=chicago_salaries user={getpass.getuser()}")
 print("Connected!")
 cursor = connection.cursor()
-cursor.execute(drop_properties_table())
+cursor.execute(drop_properties_table()) # Might comment this out after the first run
 print("Dropped Table!")
-cursor.execute(table_creation_query())
+cursor.execute(table_creation_query()) # Might comment this out after the first run
 print("Created Table")
+
+# Dict Reader to read from CSV:
+with open('chicago_salaries.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        cleaned_data = clean_data(row)
+        cursor.execute("INSERT INTO employees (first_name, last_name, job_title, full_or_part_time, department, annual_salary) VALUES (%s,%s,%s,%s,%s,%s)", (
+            cleaned_data['first_name'],
+            cleaned_data['last_name'],
+            cleaned_data['job_title'],
+            cleaned_data['full_or_part_time'],
+            cleaned_data['department'],
+            cleaned_data['annual_salary'])
+        )
+
+    # return ("CREATE TABLE employees (id serial PRIMARY KEY,  varchar(255),  varchar(255),  varchar(60),  varchar,  varchar(255),  int);")
+        #  cursor.execute("INSERT INTO properties (street_address, city, zip_code, state, number_of_beds, number_of_baths, square_feet, property_type, sale_date, sale_price, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (
+#             cleaned_data['first_name'],
+#             cleaned_data['last_name'],
+#             cleaned_data['job_title'],
+#             cleaned_data['full_or_part_time'],
+#             cleaned_data['department'],
+#             cleaned_data['annual_salary']))
+
+#             cleaned_data['square_feet'],
+#             cleaned_data['property_type'],
+#             cleaned_data['sale_date'],
+#             cleaned_data['sale_price'],
+#             cleaned_data['latitude'],
+#             cleaned_data['longitude']))
+
+connection.commit()
+connection.close()
