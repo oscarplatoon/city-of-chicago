cd47e4e0da2d66d4439624dd64504fc683573f92
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-24 17:42:08 -0500

    Jonas' and Reno's attempt at city of chicago challenge

--- chicago_queries.sql
+++ chicago_queries.sql
@@ -1,1 +1,10 @@
 -- Write your Queries here
+DROP TABLE if EXISTS employees;
+CREATE TABLE employees (
+  first_name  VARCHAR(255) NOT NULL,
+  last_name   VARCHAR(255) NOT NULL,
+  job_title   VARCHAR(255) NOT NULL,
+  full_or_part_time VARCHAR(255) NOT NULL,
+  department VARCHAR(255) NOT NULL,
+  annual_salary INTEGER
+);
