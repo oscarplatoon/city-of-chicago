5c0e5f2d29633ea053769818eced6d574e835042
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-24 21:01:03 -0400

    Andrew and Alexander_chicago salaries_final

--- chicago.py
+++ chicago.py
@@ -9,11 +9,11 @@ import csv
 
 def clean_data(csv_row):
     cleaned = {}
-    cleaned['first_name'] = csv_row[1].strip().split(' ')[0]
-    cleaned['last_name'] = csv_row[0].strip()
-    cleaned['job_title'] = csv_row[2].strip()
-    cleaned['full_or_part_time'] = csv_row[4].strip()
-    cleaned['department'] = csv_row[3].strip()
+    cleaned['first_name'] = csv_row[1].strip().split(' ')[0].replace("'","")
+    cleaned['last_name'] = csv_row[0].strip().replace("'","")
+    cleaned['job_title'] = csv_row[2].strip().replace("'","")
+    cleaned['full_or_part_time'] = csv_row[4].strip().replace("'","")
+    cleaned['department'] = csv_row[3].strip().replace("'","")
     if csv_row[5].strip() == 'Salary':
         cleaned['annual_salary'] = csv_row[7].strip()
     elif csv_row[5].strip()  == 'Hourly':
@@ -21,8 +21,8 @@ def clean_data(csv_row):
     else:
         return(print(f'Error with cleaning, csv_row[5] = {csv_row[5]}'))
     return(
-        f"{cleaned['first_name']}",f"{cleaned['last_name']}",f"{cleaned['job_title']}",
-        f"{cleaned['full_or_part_time']}",f"{cleaned['department']}",f"{cleaned['annual_salary']}"
+        f"'{cleaned['first_name']}'",f"'{cleaned['last_name']}'",f"'{cleaned['job_title']}'",
+        f"'{cleaned['full_or_part_time']}'",f"'{cleaned['department']}'",f"'{cleaned['annual_salary']}'"
         )
 
 try:
@@ -36,13 +36,14 @@ else:
     print('Success!')
     #create employees table
     insert_employee = "insert into employees(first_name,last_name,job_title,full_or_part_time,department,annual_salary) values (%s,%s,%s,%s,%s,%s)"
-    drop_employees_table = "drop table if exists employees"
-    create_employees_table = "create table employees (id serial primary key, first_name varchar(255) not null, last_name varchar(255) not null, job_title varchar(255) not null, full_or_part_time varchar(1) not null, department varchar(255), annual_salary int not null)"
+    drop_employees_table = "drop table if exists employees cascade"
+    create_employees_table = "create table employees (id serial primary key, first_name varchar(255) not null, last_name varchar(255) not null, job_title varchar(255) not null, full_or_part_time varchar(1) not null, department varchar(255), annual_salary varchar(255) not null)"
 
     cursor.execute(drop_employees_table)
     cursor.execute(create_employees_table)
+    connection.commit()
 
-# try:
+try:
     
     data = []
     columns = []
@@ -54,14 +55,16 @@ else:
                 line_count +=1
                 continue
             else:
-                print((insert_employee)%(clean_data(", ".join(row).split(','))) )
+                cursor.execute((insert_employee)%(clean_data(", ".join(row).split(','))) )
+                connection.commit()
                 #cursor.execute((insert_employee)%(clean_data(", ".join(row).split(','))) )
-# except: 
-#     print('Error loading data')
-# else:
+except: 
+    print('Error loading data')
+else:
+    print('Success')
 
-    connection.commit()
-    # connection.close()
+
+
 
 
 
